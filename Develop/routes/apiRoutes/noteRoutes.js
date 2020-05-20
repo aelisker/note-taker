3cccc612110298e8090ad9d63c9a8b75d6b0a731
Aaron Lisker <aelisker@gmail.com>
2020-05-19 22:51:27 -0700

    Able to load existing notes and save new notes - need to work on delete

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -3,14 +3,38 @@ const { notes } = require('../../db/db.json');
 const path = require("path");
 const fs = require("fs");
 
+let noteArr = []
+
 router.get('/notes', (req, res) => {
     console.log('Loading notes');
-    res.sendFile(path.join(__dirname, '../../db/db.json'));
+    noteArr = JSON.parse(fs.readFileSync(path.join(__dirname, '../../db/db.json')));
+    console.log('Notes assigned to array');
+    console.log(noteArr);
+    res.json(noteArr);
 });
 
 router.post('/notes', (req, res) => {
     console.log('Save has been clicked');
     console.log(req.body);
+
+    let highestId = 0;
+    const localNoteArr = noteArr;
+    if (localNoteArr.length > 0) {
+        localNoteArr.forEach(note => {
+            if (note.id > highestId) {
+                console.log(`Note ID: ${note.id}`);
+                console.log(`Highest ID: ${highestId}`);
+                highestId = note.id;
+            }
+        });
+    }
+    req.body.id = highestId + 1;
+
+    localNoteArr.push(req.body);
+    console.log(localNoteArr);
+
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(localNoteArr));
+    res.json(localNoteArr);
 })
 
 // router.post('/animals', (req, res) => {
