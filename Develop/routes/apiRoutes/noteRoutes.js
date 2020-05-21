4a140c958ade2768b93be250ed522d6902882637
Aaron Lisker <aelisker@gmail.com>
2020-05-20 19:38:32 -0700

    added README, just need to add video walkthrough and deploy to Heroku

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -5,20 +5,14 @@ const fs = require("fs");
 let noteArr = []
 
 router.get('/notes', (req, res) => {
-    console.log('Loading notes');
     // read json file from db, set as noteArr
     noteArr = JSON.parse(fs.readFileSync(path.join(__dirname, '../../db/db.json')));
-    console.log('Notes assigned to array');
-    console.log(noteArr);
 
     // send array back as json response object
     res.json(noteArr);
 });
 
 router.post('/notes', (req, res) => {
-    console.log('Save has been clicked');
-    console.log(req.body);
-
     // avoid global when possible
     const localNoteArr = noteArr;
 
@@ -28,8 +22,6 @@ router.post('/notes', (req, res) => {
     if (localNoteArr.length > 0) {
         localNoteArr.forEach(note => {
             if (note.id > highestId) {
-                console.log(`Note ID: ${note.id}`);
-                console.log(`Highest ID: ${highestId}`);
                 highestId = note.id;
             }
         });
@@ -39,7 +31,6 @@ router.post('/notes', (req, res) => {
 
     // push new note (request body) to note array
     localNoteArr.push(req.body);
-    console.log(localNoteArr);
 
     // save stringified json to db, send array back as response object
     fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(localNoteArr));
@@ -51,10 +42,8 @@ router.delete('/notes/:id', (req, res) => {
     // avoid global when possible
     const localNoteArr = noteArr;
     const idToDelete = Number(req.params.id); 
-    console.log(`idToDelete: ${idToDelete}`);
 
     const arrWithoutDeletedNote = localNoteArr.filter(note => note.id !== idToDelete);
-    console.log(`Show array without note that has been chosen for deletion: ${arrWithoutDeletedNote}`);
 
     // if json file exists, delete
     // found information on deleting on last comment on issue https://github.com/nodejs/node-v0.x-archive/issues/4965
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Note Taker Starter Code
+# Note Taker ![MIT-license](./utils/licenses/img/license-MIT-brightgreen.svg)
+
+## Description
+Application to take notes created using both front and backend Javascript, utilizing Node.js and Express. Application is deployed to Heroku.
+
+## Table of Contents
+* [Installation](#Installation)
+  
+* [Usage](#Usage)
+
+* [License](#License)
+
+* [Tests](#Tests)
+
+* [Questions](#Questions)
+
+## Installation
+Installation instructions listed below:
+```
+npm init, npm install
+```
+  
+## Usage
+After Node Package Manager has been initialized and installed, use npm start to start the server on port 3001.
+
+## License
+A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+To view the full license, [click here](./utils/licenses/text/MIT.txt).
+
+## Tests
+Try adding and deleting notes on an Express instance to see usage.
+  
+## Questions
+If you have any questions, you can find me at [aelisker](https://github.com/aelisker) and via email at [alisker@protonmail.com](mailto:alisker@protonmail.com).
